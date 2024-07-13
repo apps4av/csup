b4e292974222cfc31783b7ddfc06513cf245ea10
zkhan <apps4av@gmail.com>
2024-07-13 16:41:14 -0400

    added auto cycle

--- cycle.py
+++ cycle.py
@@ -1,4 +1,39 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+
+
+# returns (cycle 28, cycle 56)
+# use 1 as future to get future cycle, and -1 for past, can be any number +-n
+def calculate_cycle(future=0):
+
+    # starting conditions from a known date
+    start_utc = datetime(2020, 1, 2, 9, 0, 0, 0, timezone.utc)
+    cycle = 1
+    last_year = 2019
+    combined = 2001
+    is56 = True
+
+    # all in UTC and current date, use future to current time
+    now_utc = datetime.now(timezone.utc) + future * timedelta(28)
+
+    # find all cycles through a loop
+    while start_utc < now_utc:
+        if last_year != start_utc.year:
+            cycle = 1
+            last_year = start_utc.year
+        else:
+            cycle = cycle + 1
+
+        combined = (start_utc.year % 2000) * 100 + cycle
+        is56 = not is56
+
+        start_utc = start_utc + timedelta(days=28)
+
+    if is56:
+        return combined, combined
+    else:
+        # this is not 56, last must be, get it. should not recurse
+        x, y = calculate_cycle(future - 1)
+        return combined, x
 
 
 def get_cycle():
