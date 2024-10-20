4e327bb7932cd416b32fb68a9b9081dca6e64897
zkhan <apps4av@gmail.com>
2024-10-20 13:26:37 -0400

    change regex for better faa result

--- csup.py
+++ csup.py
@@ -4,7 +4,8 @@ import cycle
 
 start_date = cycle.get_version_start(cycle.get_cycle_download())  # to download which cycle
 
-all_charts = common.list_crawl("https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dafd/", "^http.*DCS_.*zip$")
+all_charts = common.list_crawl("https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dafd/",
+                               "^http.*DCS_" + start_date.replace("-", "") + ".zip$")
 # download
 common.download_list(all_charts)
 
